9bb6811c567e46c851c84a76e332b45f422ec611
tkennedy-fasterway <132701481+tkennedy-fasterway@users.noreply.github.com>
2024-01-24 16:59:51 -0500

    Spacing updates

--- get-referral-id.js
+++ get-referral-id.js
@@ -2,23 +2,23 @@ function getReferralID() {
 
 	var extractedReferralID = "";
   
-	// Get url param aid if exists
-  var urlParams = new URLSearchParams(window.location.search);
-  var aid = urlParams.get('aid');
-    
-  // Get anchor # if exists
-  var currentUrl = window.location.href;
-  var url = new URL(currentUrl);
-  var anchorValue = url.hash;
-  if (anchorValue.startsWith('#')) {
-      anchorValue = anchorValue.substring(1);
-  }
-  
-  if(aid) {
-  	extractedReferralID = aid;
-  } else if(anchorValue) {
-  	extractedReferralID = anchorValue;
-  }
-  
-  return extractedReferralID;
+    // Get url param aid if exists
+    var urlParams = new URLSearchParams(window.location.search);
+    var aid = urlParams.get('aid');
+
+    // Get anchor # if exists
+    var currentUrl = window.location.href;
+    var url = new URL(currentUrl);
+    var anchorValue = url.hash;
+    if (anchorValue.startsWith('#')) {
+        anchorValue = anchorValue.substring(1);
+    }
+
+    if(aid) {
+    extractedReferralID = aid;
+    } else if(anchorValue) {
+    extractedReferralID = anchorValue;
+    }
+
+    return extractedReferralID;
 }
